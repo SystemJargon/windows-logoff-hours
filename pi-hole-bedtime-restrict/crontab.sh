0bd528de01c25673194c999a03289e592a16dc2d
L <lwsnz@users.noreply.github.com>
2022-04-29 12:33:39 +1200

    Delete crontab.sh

--- pi-hole-bedtime-restrict/crontab.sh
+++
@@ -1,5 +0,0 @@
-0 22 * * 0-4 sqlite3 /etc/pihole/gravity.db "update 'group' set enabled = 1 where name = 'bedtime-restrict'"
-
-0 06 * * 1-5 sqlite3 /etc/pihole/gravity.db "update 'group' set enabled = 0 where name = 'bedtime-restrict'"
-
-0 06 * * 1-5 PATH="$PATH:/usr/sbin:/usr/local/bin/" pihole restartdns
