b84bceb7099d298937d288082cd5ce71aa91cc99
L <lwsnz@users.noreply.github.com>
2022-05-01 13:39:37 +1200

    Update set-kids-logoff-time.ps1

--- windows_logon_hours/set-kids-logoff-time.ps1
+++ windows_logon_hours/set-kids-logoff-time.ps1
@@ -12,9 +12,17 @@
       Author : https://github.com/lwsnz
 #>
 
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' '-NoProfile -WindowStyle Hidden -command "&(shutdown /l /f)"'
-$trigger =  New-ScheduledTaskTrigger -Daily -At 9pm
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Log Off Kids - Time Elapsed" -Description "Log Off Kids each day at set time. Time Elapsed" -User kids
+# Create and register the scheduled task. 
+# WE NEED TO FORCE THE LOGOUT AND SO 'Logon Hours Allowed' below works in conjunction with this.
+# Set variables so we can easily change this or another parent friend without breaking the script
+$username = "kids"
+$time = "9pm"
 
-# Also set this for ;logon hours allowed'.
+# DO NOT EDIT BELOW THIS LINE
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' '-NoProfile -WindowStyle Hidden -command "&(shutdown /l /f)"'
+$trigger =  New-ScheduledTaskTrigger -Daily -At $time
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Log Off Kids - Time Elapsed" -Description "Log Off Kids each day at set time. Time Elapsed" -User $username
+
+# YOU MAY EDIT THIS IF YOU WISH. THIS WILL PREVENT THE USER OF A LOGIN BUT ONCE AT THE LOGON SCREEN. NOT IF ALREADY LOGGED IN.
+# Also set this for 'logon hours allowed'.
 net user kids /time:"Sa-Su,8am-9pm;M-F,4pm-9pm"
